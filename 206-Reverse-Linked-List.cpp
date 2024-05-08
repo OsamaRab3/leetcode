7280c31981987450b7e38f36d23fb0365b1f8ac2
Osama Rabea <159753803+OsamaRab3@users.noreply.github.com>
2024-05-08 16:55:31 +0300

    [206] [Time Beats: 79.51%] [Memory Beats: 31.90%] - LeetPush

---
+++ 206-Reverse-Linked-List.cpp
@@ -0,0 +1,23 @@
+// class ListNode {
+// public:
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* current = head;
+        ListNode* prev = nullptr;
+        while (current != nullptr) {
+            ListNode* nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev; // new head of the reversed list
+    }
+};
