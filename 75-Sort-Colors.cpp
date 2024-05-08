15540d1ddf1d5db41e9838510721dd05ad2ff187
Osama Rabea <159753803+OsamaRab3@users.noreply.github.com>
2024-05-08 17:12:49 +0300

    [75] [Time Beats: 100.00%] [Memory Beats: 32.80%] - LeetPush

---
+++ 75-Sort-Colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=nums.size();
+        for(int i=0;i<l;i++)
+        {
+            for (int j=0;j<l-i-1;j++)
+            {
+                if (nums[j]>nums[j+1])
+                    swap(nums[j],nums[j+1]);
+            }
+
+        }
+        
+    }
+};
