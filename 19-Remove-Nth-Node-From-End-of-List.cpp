a75aca6d6a332490d0bae24bf13466f00f2b46bc
Osama Rabea <159753803+OsamaRab3@users.noreply.github.com>
2024-06-27 04:47:07 +0300

    [19] [Time Beats: 62.45%] [Memory Beats: 62.87%] - LeetPush

--- 19-Remove-Nth-Node-From-End-of-List.cpp
+++ 19-Remove-Nth-Node-From-End-of-List.cpp
@@ -1,17 +1,4 @@
-#ifndef LISTNODE_H
-#define LISTNODE_H
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode* next) : val(x), next(next) {}
-};
-
-#endif
-
- 
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
