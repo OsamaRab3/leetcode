ea1033859335a4e4a902bd9c253fc414fccdacb1
Osama Rabea <159753803+OsamaRab3@users.noreply.github.com>
2024-07-05 00:38:30 +0300

    [3110] [Time Beats: 100.00%] [Memory Beats: 96.09%] - LeetPush

--- String/3110-Score-of-a-String.cpp
+++ String/3110-Score-of-a-String.cpp
@@ -4,12 +4,10 @@ public:
     {
         int l=s.length();
         int sum=0;
-        int sa=0;
+
         for (int i=0;i<l-1;i++)
-        {
-            sa=abs((s[i]-'0')-(s[i+1]-'0'));
-            sum+=sa;
-        }
+            sum+=abs((s[i]-'0')-(s[i+1]-'0'));
+    
         return sum;
     }
 };
