43d399207b2b3eeb26523245fddbad0d136821c4
Osama Rabea <159753803+OsamaRab3@users.noreply.github.com>
2025-01-22 23:19:02 +0200

    [1] [Time Beats: 79.54%] [Memory Beats: 20.69%] - LeetPush

---
+++ 1-Two-Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, int> u_map;
+        int len = nums.size();
+
+        for (int i = 0; i < len; ++i) {
+            int complement = target - nums[i];
+            if (u_map.find(complement) != u_map.end()) {
+                return {u_map[complement], i};
+            }
+            u_map[nums[i]] = i;
+        }
+
+        return {};
+    }
+    
+};
